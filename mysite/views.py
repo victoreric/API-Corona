d01e25261f63f551915cd632f460c18df0675e53
victorEric <victoreric1409@gmail.com>
2020-11-08 18:09:59 +0700

    inisialisasi commit

---
+++ mysite/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+import requests
+
+def home(request):
+    response = requests.get('https://api.kawalcorona.com/indonesia/provinsi')
+    data_prov = response.json()
+    case_prov = []
+    for prov in data_prov :
+        result_prov = {}
+        result_prov['kode_prov' ] = prov['attributes']['Kode_Provi']
+        result_prov['provinsi' ] = prov['attributes']['Provinsi']
+        result_prov['kasus_positif'] = prov['attributes']['Kasus_Posi']
+        result_prov['kasus_sembuh'] = prov ['attributes']['Kasus_Semb']
+        result_prov['kasus_meninggal'] = prov['attributes']['Kasus_Meni']
+        case_prov.append(result_prov)
+
+
+    response_indo = requests.get('https://api.kawalcorona.com/indonesia')
+    data_indo = response_indo.json()
+    case_indo = []
+    for d in data_indo :
+        result = {}
+        result['name'] = d['name']
+        result['positif'] = d['positif']
+        result['sembuh'] = d['sembuh']
+        result['meninggal'] = d['meninggal']
+        case_indo.append(result)
+        
+    context = {
+        'Title' : 'API-Corona',
+        'Judul' : 'Corona Virus Cases in Indonesia',
+        'data' : case_prov,
+        'case_indo' : case_indo 
+    }
+    return render (request,'index.html', context)
+
+
